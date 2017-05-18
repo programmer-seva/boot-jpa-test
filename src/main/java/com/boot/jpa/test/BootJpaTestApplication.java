9e61fab98e666b06faef4f4a305ba50b3bc42b2d
programmer-seva <sooda891118@naver.com>
2017-05-19 02:00:58 +0900

    initial project commit test

---
+++ src/main/java/com/boot/jpa/test/BootJpaTestApplication.java
@@ -0,0 +1,12 @@
+package com.boot.jpa.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootJpaTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootJpaTestApplication.class, args);
+	}
+}
